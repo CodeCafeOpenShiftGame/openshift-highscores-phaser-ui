b5d10f0db1df2206ed87a8ab0c7ea9b545b00f83
mwcz <mwc@clayto.com>
2020-10-26 12:34:29 -0400

    add trailing slash

--- webpack/base.js
+++ webpack/base.js
@@ -3,7 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const publicPath = '/pod-escape-leaderboard';
+const publicPath = '/pod-escape-leaderboard/';
 
 module.exports = {
   mode: "development",
