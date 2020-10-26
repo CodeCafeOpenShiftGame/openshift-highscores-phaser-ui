264a022bdbbb3f875e8051e2223b31aa3a6f8726
mwcz <mwc@clayto.com>
2020-10-26 11:51:21 -0400

    public path /pod-escape-leaderboard

--- webpack/base.js
+++ webpack/base.js
@@ -6,6 +6,9 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   mode: "development",
   devtool: "eval-source-map",
+  devServer: {
+      publicPath: '/pod-escape-leaderboard/'
+  },
   module: {
     rules: [
       {
